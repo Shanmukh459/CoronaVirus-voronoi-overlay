a7d4e6e10ea7504846bd6abe743755efb2358d97
Shanmukh459 <saikrrish459@gmail.com>
2024-06-02 12:41:48 -0700

    voronois implemented

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -1,7 +1,8 @@
-import { line, scaleLog, scaleTime, extent, max } from 'd3'
-import React from 'react'
-import { XAxis } from './XAxis'
-import { YAxis } from './YAxis'
+import { line, scaleLog, scaleTime, extent, max } from "d3"
+import React, { useCallback, useMemo } from "react"
+import { XAxis } from "./XAxis"
+import { YAxis } from "./YAxis"
+import { VoronoiOverlay } from "./VoronoiOverlay"
 
 const epsilon = 1
 
@@ -9,53 +10,78 @@ const margin = {
   top: 60,
   right: 20,
   bottom: 60,
-  left: 70
+  left: 70,
 }
-export const LineChart = ({data, width, height}) => {
+
+const xValue = (d) => d.date
+const yValue = (d) => d.totalDeaths
+
+export const LineChart = ({ data, width, height }) => {
   const innerWidth = width - margin.left - margin.right
   const innerHeight = height - margin.top - margin.bottom
 
-  const allData = data.flat(Infinity)
+  const allData = useMemo(() => data.flat(Infinity), [data])
 
-  const xValue = d => d.date
-  const xScale = scaleTime()
-    .domain(extent(allData, xValue))
-    .range([0, innerWidth])
+  const xScale = useMemo(
+    () => scaleTime()
+      .domain(extent(allData, xValue))
+      .range([0, innerWidth]),
+    [allData, xValue, innerWidth]
+  )
 
-  const yValue = d => d.totalDeaths
-  const yScale = scaleLog()
-    .domain([epsilon, max(allData, yValue)])
-    .range([innerHeight, 0])
+  const yScale = useMemo(
+    () =>
+      scaleLog()
+        .domain([epsilon, max(allData, yValue)])
+        .range([innerHeight, 0]),
+    [epsilon, allData, yValue, innerHeight]
+  )
 
-  const lineGenerator = line()
-    .x(d => xScale(xValue(d)))
-    .y(d => yScale(epsilon+yValue(d)))
-  
+  const lineGenerator = useMemo(
+    () =>
+      line()
+        .x((d) => xScale(xValue(d)))
+        .y((d) => yScale(epsilon + yValue(d))),
+    [xScale, xValue, yScale, yValue]
+  )
+
+  const handleVoronoiHover = useCallback(() => {
+    console.log("Hovered!")
+  }, [])
+
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <text
-          className='title'
-          x={innerWidth/2}
-          textAnchor='middle'
-          y={-20}
-        >Global Corona Virus Deaths Over Time By Country</text>
+        <text className="title" x={innerWidth / 2} textAnchor="middle" y={-20}>
+          Global Corona Virus Deaths Over Time By Country
+        </text>
         <XAxis xScale={xScale} innerHeight={innerHeight} />
         <text
-          className='axis-label'
-          alignmentBaseline='hanging'
-          y={innerHeight+25}
-          x={innerWidth/2}
-        >Time</text>
+          className="axis-label"
+          alignmentBaseline="hanging"
+          y={innerHeight + 25}
+          x={innerWidth / 2}
+        >
+          Time
+        </text>
 
         <YAxis yScale={yScale} innerWidth={innerWidth} />
         <text
-          className='axis-label'
-          transform={`translate(-35, ${innerHeight/2}) rotate(-90)`}
-        >Cummulative Deaths</text>
-        {data.map(countryTimeSeries => (
-          <path className='marker-line' d={lineGenerator(countryTimeSeries)} />
+          className="axis-label"
+          transform={`translate(-35, ${innerHeight / 2}) rotate(-90)`}
+        >
+          Cummulative Deaths
+        </text>
+        {data.map((countryTimeSeries) => (
+          <path className="marker-line" d={lineGenerator(countryTimeSeries)} />
         ))}
+        <VoronoiOverlay
+          allData={allData}
+          lineGenerator={lineGenerator}
+          innerWidth={innerWidth}
+          innerHeight={innerHeight}
+          onHover={handleVoronoiHover}
+        />
       </g>
     </svg>
   )
